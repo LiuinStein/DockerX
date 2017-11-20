e1aa737b3580762c446f81a529877d1717fd80ac
LiuinStein PC <liuinstein@outlook.com>
2017-11-20 14:48:54 +0800

    first commit

---
+++ DockerX/Version.h
@@ -0,0 +1,7 @@
+#pragma once
+
+const char * g_ver[] = {
+	R"(Version:      1.0.0)",
+	R"(Built:        Thu Nov  2 20:40:23 2017)",
+	R"(OS/Arch:      linux/amd64)"
+};
--- DockerX/main.cpp
+++ DockerX/main.cpp
@@ -1,7 +1,45 @@
-#include <cstdio>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "Help.h"
+#include "Version.h"
+#include <algorithm>
 
-int main()
+int main(int argc, char * argv[])
 {
-    printf("hello from DockerX!\n");
-    return 0;
+	if (argc == 1)
+	{
+		// no input params
+		std::cout << g_HelpDocs[DOCS_GLOBAL] << std::endl;
+		return 0;
+	}
+
+	// load additional function 
+	if (argv[1] == std::string{ "registry" })
+	{
+
+
+	}
+	else if (argv[1] == std::string{ "version" } || argv[1] == std::string{ "-v" } || argv[1] == std::string{ "--version" })
+	{
+		// output version information
+		std::cout << "DockerX:" << std::endl;
+		for (auto var : g_ver)
+		{
+			std::cout << var << std::endl;
+		}
+		std::cout << std::endl;
+		system(("docker " + std::string{ argv[1] }).c_str());
+	}
+	else
+	{
+		// send request to docker
+		std::string cmd;
+		for (int i = 1; i < argc; i++)
+		{
+			cmd += argv[i] + std::string{ " " };
+		}
+		system(("docker " + cmd).c_str());
+	}
+	return 0;
 }
